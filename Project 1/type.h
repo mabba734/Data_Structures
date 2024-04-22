541652f894d71cb6dfc51010cd1756be92f94e4c
Marim Abbas <115725836+mabba734@users.noreply.github.com>
2024-04-21 19:46:57 -0500

    Create type.h

---
+++ Project 1/type.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// Declare the student type in this header file
+// Include this file in your main program file
+
+struct StudentType {
+  std::string ID;
+  std::string Name;
+  int CLA;
+  int OLA;
+  int Quiz;
+  int Homework;
+  int Exam;
+  int Bonus;
+};
