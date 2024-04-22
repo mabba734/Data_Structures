ceae6d045b85bb0f5354af6c4c8d09657c846723
Marim Abbas <115725836+mabba734@users.noreply.github.com>
2024-04-21 19:57:48 -0500

    Create type.h

---
+++ Project_4/type.h
@@ -0,0 +1,30 @@
+// DO NOT CHANGE THIS FILE
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#ifndef Type_H
+#define Type_H
+
+// define struct flightRec
+struct flightRec {
+  string origin;
+  int flightNum;
+  string destination;
+  int price;
+
+  bool operator<(const flightRec &rhs) const;
+  // overloaded < operator for flightRec
+  // this function compares the destinations between two flightRec
+
+  bool operator==(const flightRec &rhs) const;
+  // overloaded == operator for flightRec
+  // this function compares the origins and destinations between two flightRec
+
+  flightRec &operator=(const flightRec &rhs);
+
+  friend ostream &operator<<(ostream &os, const flightRec &f);
+  // overloaded output operator for flightRec struct
+};
+
+#endif
